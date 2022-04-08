b33dbdca497494d3b02510c181f395defd202197
ashwin kavidasan <95648290+ashwinkkkk@users.noreply.github.com>
2022-04-08 10:41:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 cp ./ssmtp.conf /etc/ssmtp/ssmtp.conf
 cp ./revaliases /etc/ssmtp/revaliases
 apt-get install ruby-dev
-apt-get install sendmail
+apt-get install ssmtp
 gem install wpscan
 touch /var/spool/cron/root
 echo "58 15 28 * * ./main.sh" >> /var/spool/cron/root
